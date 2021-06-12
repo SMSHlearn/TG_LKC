ac8f1064bb69e8ad38fee4227ab2e371dfce3715
Cason <casongg@gmail.com>
2021-06-12 10:17:02 +0800

    crawler

--- main.py
+++ main.py
@@ -13,15 +13,13 @@ Callback Function
 """
 # Google Drive Callback
 def drive_handler(update: Update, context: CallbackContext) -> None:
-    bot.send_chat_action(chat_id = update.message.chat_id , action = telegram.ChatAction.TYPING) # 會顯示chatbot正在輸入中，增加對話真實感
-    time.sleep(1) # 在顯示輸入中後停頓1秒，然後顯示下一句code的文字
     context.bot.send_message(chat_id=update.effective_chat.id , text=drive_msg , parse_mode='HTML', disable_web_page_preview=True)
 
 def setdrive_handler(update: Update, context: CallbackContext) -> None:
     #mongodb
     #
     # local
-    if os.path.isfile(f'user_credentials/{update.effective_chat.id}.json'):
+    if os.path.isfile(f'json/{update.effective_chat.id}.json'):
         context.bot.send_message(chat_id=update.effective_chat.id, text='您已授權過機器人囉！')
         return
 
@@ -40,7 +38,7 @@ def unsetdrive_handler(update:Update , context:CallbackContext) -> None:
         # mongodb
         #
         # local
-        os.remove(f'user_credentials/{update.effective_chat.id}.json')
+        os.remove(f'json/{update.effective_chat.id}.json')
         context.bot.send_message(chat_id=update.effective_chat.id, text='已取消授權！')
     except Exception as e:
         context.bot.send_message(chat_id=update.effective_chat.id, text='您還沒設定授權哦！')
@@ -48,43 +46,50 @@ def unsetdrive_handler(update:Update , context:CallbackContext) -> None:
 
 #start
 def start_handler(update: Update, context: CallbackContext) -> None:
-    bot.send_chat_action(chat_id = update.message.chat_id, action = telegram.ChatAction.TYPING) # 會顯示chatbot正在輸入中，增加對話真實感
-    time.sleep(1) # 在顯示輸入中後停頓1秒，然後顯示下一句code的文字
     context.bot.send_message(chat_id=update.effective_chat.id , text=help_msg , parse_mode='HTML', disable_web_page_preview=True)
 
 
+ # 開發者資料 Callback
+def info_handler(update:Update , context:CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id , text=info_msg , parse_mode='HTML', disable_web_page_preview=True)
+
 # 推播管理相關callback
-# def list_handler() -> None:
+def list_handler(update:Update , context:CallbackContext) -> None:
+    keywords = have_keywords(update, context)
+    if keywords:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='您目前推播的搜尋關鍵字有：\n'+'\n'.join(keywords))
 
 
-# def delete_handler() -> None:
+def delete_handler(update:Update , context:CallbackContext) -> None:
+    keywords = have_keywords(update, context)
+    if keywords:
+        keyboard = [[InlineKeyboardButton(keyword, callback_data=i)] for i, keyword in enumerate(keywords)]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text='請選取要取消推播的關鍵字', reply_markup=reply_markup)
 
 # def button(update:Update, _:CallbackContext):
 #     index = int(user_data[str(update.effective_chat.id)])
 #     query = update.callback_query
 #     keyword, push_time = user_data['users'][index]['keywords'][int(query.data)]
-#     # user_data['users'][index]['keywords'].pop(int(query.data))
-#     # update_user_data(update.effective_chat.id, None, None)
+#     user_data['users'][index]['keywords'].pop(int(query.data))
+#     update_user_data(update.effective_chat.id, None, None)
 
 #     sd.clear(f'{update.effective_chat.id}_{keyword}_{push_time}')
 
 #     query.answer()
 #     query.edit_message_text(text=f'已刪除【{keyword} @{push_time}】')
 
-# def serch_handler(update:Update , context:CallbackContext) -> None:
-#     if context.args == []:
-#         context.bot.send_message(chat_id=update.effective_chat.id, text='請在 /search 後輸入搜尋關鍵字')
-#         return
-#     keyword = ' '.join(context.args)
-#     bot.send_message(chat_id=update.effective_chat.id, text=f'正在爬取【{keyword}】，請稍待...\n')
-#     csv_data = mhj
 
-#      if csv_data.__len__() == 0:
-#         context.bot.send_message(chat_id=update.effective_chat.id, text='此關鍵字今日無更新職缺')
-#     else:
-#         for job in islice(csv_data, 1, None):
-#             context.bot.send_message(chat_id=update.effective_chat.id,
-#                                      disable_web_page_preview=True, text='\n'.join(job))
+
+def search_handler(update:Update , context:CallbackContext) -> None:
+    if context.args == []:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='請在 /search 後輸入搜尋關鍵字')
+        return
+    keyword = ' '.join(context.args)
+    bot.send_message(chat_id=update.effective_chat.id, text=f'正在爬取【{keyword}】，請稍待...\n')
+    csv_data = utils.stock_crawler(keyword, update.effective_chat.id)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=csv_data)
 
 
 
@@ -106,19 +111,29 @@ def start_handler(update: Update, context: CallbackContext) -> None:
 #         context.bot.send_message(chat_id=update.effective_chat.id,
 #                                  text=f'時間格式有誤哦！請重新輸入\n格式為24小時制，缺空須補0，如09:05')
 
-
-
-def info_handler(update:Update , context:CallbackContext) -> None:
-    bot.send_chat_action(chat_id = update.message.chat_id , action = telegram.ChatAction.TYPING) # 會顯示chatbot正在輸入中，增加對話真實感
-    time.sleep(1) # 在顯示輸入中後停頓1秒，然後顯示下一句code的文字
-    context.bot.send_message(chat_id=update.effective_chat.id , text=info_msg , parse_mode='HTML', disable_web_page_preview=True)
-
-
+# 看不懂的訊息
+def unknown(update:Update, context:CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text='機器人看不懂您說什麼\n請用 /help 觀看使用說明')
 
 
 """
 Other Function
 """
+
+def have_keywords(update, context):
+    try:
+        index = int(user_data[str(update.effective_chat.id)])
+    except Exception as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您目前沒有排程任何關鍵字")
+        return None
+    keywords = user_data['users'][index]['keywords']
+    if keywords.__len__() == 0:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="您目前沒有排程任何關鍵字")
+        return None
+
+    keywords_to_display = [f'{d[0]} @{d[1]}' for d in keywords]
+
+    return keywords_to_display
 
 # CommandHandler的集合
 def command() -> None:
@@ -127,7 +142,8 @@ def command() -> None:
      # 開始與幫助指令
     dispatcher.add_handler(CommandHandler('start', start_handler))
     dispatcher.add_handler(CommandHandler('help', start_handler))
-
+    # 直接搜尋指令
+    # 推播相關指令
 
     # Google Drive相關指令
     dispatcher.add_handler(CommandHandler('drive' , drive_handler))
@@ -136,6 +152,14 @@ def command() -> None:
     dispatcher.add_handler(CommandHandler('unsetdrive' , unsetdrive_handler))
     # 開發者資料
     dispatcher.add_handler(CommandHandler('info' , info_handler))
+    # 推播管理相關
+    # dispatcher.add_handler(CommandHandler('list' , list_handler))
+    # dispatcher.add_handler(CommandHandler('delete' , delete_handler))
+    #搜尋與推播關鍵字設定
+    dispatcher.add_handler(CommandHandler('search' , search_handler))
+    # dispatcher.add_handler(CommandHandler('schedule' , schedule_handler))
+    # 看不懂的訊息
+    dispatcher.add_handler(MessageHandler(Filters.text | Filters.command, unknown))
     # Start the Bot
     updater.start_polling()
     updater.idle()
@@ -150,7 +174,7 @@ if __name__ == '__main__':
     help_msg = '歡迎使用LKC_stock股票機器人，機器人會根據你提供的股票代號或名稱，會從<b>基本市況報導網站</b>爬取當日的股票內容給您！\n' \
         '機器人具有,<b>每日推播</b>的功能與<b>自動上傳到Google Drive</b>的功能，以下為使用說明：\n\n' \
         '<b>搜尋功能與推播設定</b>\n' \
-        '/serch 2330 - 直接搜尋關鍵字 【2330 台積電】\n' \
+        '/search 2330 - 直接搜尋關鍵字 【2330 台積電】\n' \
         '/schedule 台積電 - 點選底下按鈕設定，將於每日開盤前或開盤後推播【台積電】股市資料給您\n\n' \
         '<b>管理您的推播列表</b>\n' \
         '/list - 列出推播列表\n' \
@@ -167,15 +191,28 @@ if __name__ == '__main__':
             '\n授權成功後，機器人會在您的Google Drive上建立名為『股票每日資訊』的資料夾，並把結果存進去！'
 
     info_msg = '<b>開發者</b> Developers: yang-lin94(github)\n' \
-            '<b>參與者</b> partner: leon921120(github)\n' \
+            '<b>參與者</b> Partner: leon921120(github) jimmy1214(github)\n' \
             '<b>源碼</b> Source:  https://github.com/SMSHlearn/TG_LKC\n' \
             '<b>協助</b> Support Server: https://t.me/LKC_stock_bot \n'\
             '<b>版本</b>: Version V1.0a\n\n' \
             '<b>Made with ❤️</b>'
 
 
+    # local
+    os.makedirs('json', exist_ok=True)
+
+    # local
+    # if not os.path.exists(user_data_filename):
+    #     with open(user_data_filename, 'w') as json_file:
+    #         initial_json = {'users': []}
+    #         json.dump(initial_json, json_file, indent=2)
+    # with open(user_data_filename, 'r') as f:
+    #     user_data = json.load(f)
+
+
+
+
     TOKEN = config.config.Token
     bot = telegram.Bot(token = TOKEN)
     updater = Updater(TOKEN, use_context = True)
     command()
-
--- utils.py
+++ utils.py
@@ -3,16 +3,52 @@ from pydrive.drive import GoogleDrive
 import os
 import csv
 import json
-import config.config
 import pymongo
+import pandas as pd
+import requests
+import config.config
 
-def stock_crawler():
-    print("#")
+def stock_crawler(keyword, chat_id):
+    """
+    爬取網頁並回傳csv data
+    """
+    #mongodb
+    #
+    # local
+    have_setdrive = os.path.isfile(f'user_credentials/{chat_id}.json')
+    s = "|"
+    stocks = []
+    data = []
+    csv_data = []
+    num = 0
 
-def write_csv(csv_data):
-    with open('temp.csv', 'w', encoding='utf-8-sig', errors='replace') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerows(csv_data)
+    stocks.append(keyword)
+    for x in stocks:
+        keyword = x
+        url_base = f'tse_{keyword.replace("", "")}.tw'
+        data.append(url_base)
+    try:
+        l = (s.join( data ))
+        url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch="+l
+        res = requests.get(url)
+        data = res.json()
+
+        for stocks in data:
+
+            name = data['msgArray'][num]['n']
+            accumulation_volume =  data['msgArray'][num]['v']
+            oprice = data['msgArray'][num]['o']
+            hprice = data['msgArray'][num]['h']
+            lprice = data['msgArray'][num]['l']
+        csv_data = name  + f'\n累積成交量:{accumulation_volume}' + f'\n開盤價:{oprice}' + f'\n當日最高價:{hprice}' + f'\n當日最低價:{lprice}'
+        # csv_data.append([name,f'累積成交量:{accumulation_volume}',f'開盤價:{oprice}',f'當日最高價:{hprice}',f'當日最低價:{lprice}' ])
+    except Exception as e:
+        csv_data = '查無此股票資料,請確認代碼是否輸入正確'
+
+    return csv_data
+
+
+# def write_csv(csv_data):
 
 def get_authorize_url():
     gauth = GoogleAuth()
@@ -22,14 +58,14 @@ def save_drive_credential(chat_id, code):
     try:
         gauth = GoogleAuth()
         gauth.Auth(code)
-        gauth.SaveCredentialsFile(f'{chat_id}.json')
+        gauth.SaveCredentialsFile(f'json/{chat_id}.json')
         return '授權成功！'
     except Exception as e:
         return '授權失敗！可能是格式錯了之類的QQ，麻煩再試一次'
 
 # def upload_drive(keyword, chat_id):
 
-#     # check folder是否已存在
+# #     # check folder是否已存在
 #     folder_name = '股票每日開資訊'
 #     folder_id = None
 #     folder_list = drive.ListFile({'q': "mimeType = 'application/vnd.google-apps.folder' and trashed = false"}).GetList()
@@ -37,3 +73,16 @@ def save_drive_credential(chat_id, code):
 #         if folder['title'] == folder_name:
 #             folder_id = folder['id']
 #             break
+
+#     if not folder_id:
+#         folder = drive.CreateFile({'title': folder_name, 'mimeType': 'application/vnd.google-apps.folder'})
+#         folder.Upload()
+#         folder_id = folder['id']
+
+#     date = '_'.join(map(str, time.localtime()[0:3]))
+#     filename = f'{date}_{keyword}.csv'
+
+#     # lcoal永遠都存temp.csv，上傳到雲端的再有custom filename
+#     file = drive.CreateFile({'title': filename, 'parents': [{'kind': 'drive#fileLink', 'id': folder_id}]})
+#     file.SetContentFile(filename='temp.csv')
+#     file.Upload()
